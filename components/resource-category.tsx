ff71b80518b3d4c5eef2914baec91b94ec03293a
George Bockari <bitwhys@gmail.com>
2023-04-09 02:47:21 -0400

    refactor: work on resources

--- components/resource-category.tsx
+++ components/resource-category.tsx
@@ -11,13 +11,24 @@ export type ResourceCategoryProps = {
 
 const DEFAULT_MAX_ITEMS = 9
 
+const Count = ({ count }: { count: number }) => {
+  return (
+    <span className="inline-flex items-center rounded-full border border-primary-7 bg-primary-3 px-3 py-0.5 text-xs font-medium text-primary-10 dark:border-primaryDark-7 dark:bg-primaryDark-3 dark:text-primaryDark-10">
+      {count}
+    </span>
+  )
+}
 const ResourceCategory = ({ category, resources }: ResourceCategoryProps) => {
   const showMore = resources.length > DEFAULT_MAX_ITEMS
+  const count = resources.length
   if (showMore) resources = resources.slice(0, DEFAULT_MAX_ITEMS)
   return (
     <section className="space-y-4">
       <div className="flex w-full items-center justify-between">
-        <h2 className="font-heading text-xl tracking-wide">{category}</h2>
+        <div className="flex items-center gap-x-3">
+          <h2 className="font-heading text-xl tracking-wide">{category}</h2>
+          {showMore && <Count count={count} />}
+        </div>
         <span className={clsx('cursor-pointer text-sm text-primary-10', { hidden: !showMore })}>View more</span>
       </div>
       <div className="grid grid-cols-3 gap-4 lg:gap-6">
--- components/resource.tsx
+++ components/resource.tsx
@@ -8,10 +8,10 @@ export type ResourceProps = {
 const Resource = ({ name, description }: ResourceProps) => {
   return (
     <div className="rounded-md bg-white p-5 shadow-1 ring-1 ring-transparent dark:bg-neutralDark-2 dark:shadow-none dark:ring-neutralDark-6">
-      <div className="flex h-full flex-col gap-y-4">
+      <div className="flex h-full flex-col gap-y-5">
         <div className="h-8 w-8 rounded-md border border-dashed border-neutral-7 bg-neutral-1 dark:border-neutralDark-7 dark:bg-neutralDark-3 lg:h-14 lg:w-14"></div>
-        <div className="space-y-1.5">
-          <h2 className="text-base font-bold">{name}</h2>{' '}
+        <div className="space-y-1">
+          <h2 className="text-base font-bold leading-tight">{name}</h2>{' '}
           <p className="line-clamp-3 text-sm text-neutral-11 dark:text-neutralDark-11">{description}</p>
         </div>
       </div>
